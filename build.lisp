39946721cddde8707b4061d4d66c8190d343c3f5
The Mimix Company <code@mimix.io>
2019-11-27 19:21:40 +0800

    build:hide backtrace in executables

--- build.lisp
+++ build.lisp
@@ -2,6 +2,14 @@
 
 (in-package #:engine)
 
+(defun set-debugger-hook ()
+  (setf *debugger-hook*
+        (lambda (condition hook)
+          (declare (ignore hook))
+          (format *error-output* "Caught error: ~A" condition)
+          (finish-output *error-output*)
+          (uiop:quit))))
+
 (defun build ()
   "Build the executable of the engine for different OSes."
   (let* ((suffix (cond ((uiop:os-macosx-p) "_macos")
@@ -10,4 +18,5 @@
                        (t (error "No matching OS found."))))
          (path (pathname (mof:cat "engine/engine" suffix))))
     (uiop:ensure-all-directories-exist (list (namestring path)))
+    (set-debugger-hook)
     (trivial-dump-core:save-executable path #'engine:main)))
