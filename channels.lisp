76680c9e2163e7c05c6a6ddfba879f4cddab68a3
The Mimix Company <code@mimix.io>
2020-01-06 19:33:27 +0800

    Implement basic mx-atom evaluator

--- channels.lisp
+++ channels.lisp
@@ -3,156 +3,108 @@
 (uiop:define-package #:streams/channels
     (:use #:cl)
   (:nicknames #:s/channels)
-  (:export #:make-universe
-           #:make-atxm
-           #:dump-atxm))
+  (:export #:make-mx-universe
+           #:make-mx-base
+           #:make-mx-atom
+           #:dump-mx-atom
+           #:table
+           #:id
+           #:data
+           #:metadata))
 
 (in-package #:streams/channels)
 
-;;; Note: should a universe contain all levels?
-(defclass universe ()
+;;; Note: should an MX-UNIVERSE instance contain all levels?
+(defclass mx-universe ()
   ((acounter :initarg :acounter
              :initform streams/globals:*initial-acounter*
              :accessor acounter
-             :documentation "The top-level atxm counter.")
+             :documentation "The top-level mx-atom counter.")
    (atable :initarg :atable
            :initform (make-hash-table)
            :accessor atable
-           :documentation "The top-level colletion of atxms."))
-  (:documentation "The top-level data structure for atxms including information about the current atxm counter and the main table."))
+           :documentation "The top-level colletion of mx-atoms."))
+  (:documentation "The top-level data structure for mx-atoms including information about the current mx-atom counter and the main table."))
 
-(defclass machine ()
+(defclass mx-base ()
+  ((table :initarg :table
+          :initform (make-hash-table)
+          :accessor table
+          :documentation ""))
+  (:documentation "The default store to use when there are no other available contexts."))
+
+(defclass mx-machine ()
   ((table :initarg :table
           :initform (make-hash-table)
           :accessor table
           :documentation ""))
   (:documentation ""))
 
-(defclass world ()
+(defclass mx-world ()
   ((table :initarg :table
           :initform (make-hash-table)
           :accessor table
           :documentation ""))
   (:documentation ""))
 
-(defclass strexm ()
+(defclass mx-stream ()
   ((table :initarg :table
           :initform (make-hash-table)
           :accessor table
           :documentation ""))
   (:documentation ""))
 
-(defclass view ()
+(defclass mx-view ()
   ((table :initarg :table
           :initform (make-hash-table)
           :accessor table
           :documentation ""))
   (:documentation ""))
 
-(defclass atxm ()
+(defclass mx-atom ()
   ((id :initarg :id
        :initform -1
        :reader id
-       :documentation "The unique numeric id of a atxm.")
+       :documentation "The unique numeric id of a mx-atom.")
    (category :initarg :category
              :initform nil
              :reader category
-             :documentation "The category of an atxm, whether it is machine, stream, canon, view, or basic.")
+             :documentation "The category of an mx-atom, whether it is mx-machine, mx-stream, mx-canon, mx-view, or basic.")
    (data :initarg :data
          :initform nil
          :reader data
-         :documentation "The main data of an atxm.")
+         :documentation "The main data of an mx-atom.")
    (metadata :initarg :metadata
              :initform nil
              :reader metadata
-             :documentation "The secondary data of an atxm which contains information about the DATA slot."))
-  (:documentation "The class for holding information about atxms."))
+             :documentation "The secondary data of an mx-atom which contains information about the DATA slot."))
+  (:documentation "The class for holding information about mx-atoms."))
 
-(defmacro spawn-counter (universe accessor)
-  "Generate a new counter in UNIVERSE with ACCESSOR."
-  `(progn (incf (,accessor ,universe))
-          (,accessor ,universe)))
-(defun spawn-acounter (universe)
+(defmacro spawn-counter (mx-universe accessor)
+  "Generate a new counter in MX-UNIVERSE with ACCESSOR."
+  `(progn (incf (,accessor ,mx-universe))
+          (,accessor ,mx-universe)))
+(defun spawn-acounter (mx-universe)
   "See SPAWN-COUNTER."
-  (spawn-counter universe acounter))
+  (spawn-counter mx-universe acounter))
 
-;;; Note: should the table in universe be updated to reflect the existence of the new atxm?
-(defmethod initialize-instance :after ((a atxm) &key universe)
-  "Initialize ATXM A in UNIVERSE."
-  (let ((counter (spawn-acounter universe)))
+(defmethod initialize-instance :after ((a mx-atom) &key mx-universe)
+  "Initialize MX-ATOM A in MX-UNIVERSE."
+  (let ((counter (spawn-acounter mx-universe)))
     (with-slots (id)
         a
       (setf id counter))))
 
-(defun make-universe ()
-  "Return an instance of the universe."
-  (make-instance 'universe))
+(defun make-mx-universe ()
+  "Return an instance of the mx-universe class."
+  (make-instance 'mx-universe))
 
-(defun dump-atxm (atxm)
-  "Display the contents of ATXM."
-  (loop :for slot :in (streams/common:slots atxm)
-        :do (format t "~A: ~S~%" slot (funcall slot atxm))))
+(defun make-mx-base ()
+  "Return an instance of the mx-base class."
+  (make-instance 'mx-base))
 
-(defun atxm-data-p (data)
-  "Return true if DATA is a valid atxm."
-  (or (and (symbolp data) (not (keywordp data)))
-      (stringp data)))
-
-(defun read-atxm (&rest body)
-  "Read the atxm data from BODY recursively. The first value retured is the main data while the second value returned is the metadata."
-  (labels ((fn (args data)
-             (cond ((keywordp (car args)) (values (nreverse data) args))
-                   ((null args) (values (nreverse data) nil))
-                   ((atxm-data-p (car args))
-                    (fn (cdr args) (cons (car args) data)))
-                   (t (fn (cddr args) data)))))
-    (fn body nil)))
-
-(defun assoc-key (key items)
-  "Return the key found in items if key is found."
-  (let ((val (assoc key items)))
-    (when val
-      (car val))))
-
-(defun assoc-value (key items)
-  "Return the value found in items if key is found."
-  (let ((val (assoc key items)))
-    (when val
-      (cdr val))))
-
-(defun dotted-pair-p (pair)
-  "Return true if LIST is a dotted list."
-  (cond ((atom (cdr pair)) t)
-        ((listp (cdr pair)) nil)
-        (t nil)))
-
-(defun build-pairs (items)
-  "Group items into pairs."
-  (when (evenp (length items))
-    (labels ((fn (items acc)
-               (cond ((null items) (nreverse acc))
-                     (t (fn (cddr items)
-                            (cons (list (first items) (second items))
-                                  acc))))))
-      (fn items nil))))
-
-(defun build-map (items &key (test #'keywordp))
-  "Create key-value mappings from ITEMS."
-  (loop :for item :in (build-pairs items)
-        :when (funcall test (car item))
-        :collect (cons (first item) (second item))))
-
-;;; Note: extend UNIVERSE to apply to context tables
-(defun make-atxm (category data metadata &key universe)
-  "Return a new atxm instance from arguments."
-  (make-instance 'atxm :category category
-                       :data data :metadata metadata
-                       :universe (or universe streams/globals:*universe*)))
-
-(defun build-atxm (&rest args)
-  "Return an atxm instance from args."
-  (multiple-value-bind (data metadata)
-      (apply #'read-atxm args)
-    (let ((head (build-map data :test #'symbolp))
-          (body (build-map metadata)))
-      (make-atxm 'basic head body))))
+(defun make-mx-atom (category data metadata &key mx-universe)
+  "Return a new mx-atom instance from arguments."
+  (make-instance 'mx-atom :category category
+                          :data data :metadata metadata
+                          :mx-universe (or mx-universe streams/globals:*mx-universe*)))
--- globals.lisp
+++ globals.lisp
@@ -4,12 +4,41 @@
     (:use #:cl)
   (:nicknames #:s/globals)
   (:export #:*initial-acounter*
-           #:*universe*))
+           #:*mx-universe*
+           #:*mx-machine*
+           #:*mx-world*
+           #:*mx-stream*
+           #:*mx-view*
+           #:*mx-canon*
+           #:*context*
+           #:*base-context*))
 
 (in-package #:streams/globals)
 
-(defparameter *universe* nil
-  "The top-level structure for the universe.")
-
 (defparameter *initial-acounter* 1000
-  "The initial atxm counter value.")
+  "The initial mx-atom counter value.")
+
+(defparameter *mx-universe* nil
+  "The top-level structure for the mx-universe.")
+
+(defparameter *mx-machine* nil
+  "The current machine context.")
+
+(defparameter *mx-world* nil
+  "The current world context.")
+
+(defparameter *mx-stream* nil
+  "The current stream context.")
+
+(defparameter *mx-view* nil
+  "The current view context.")
+
+(defparameter *mx-canon* nil
+  "The current canon context.")
+
+;;; Note: enable support for context chaining
+(defparameter *context* nil
+  "The immediate surrounding context for any given operation. Defaults to MX-BASE.")
+
+(defparameter *base-context* nil
+  "The fallback context for @ operations.")
