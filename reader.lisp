68d0c84059b7cf36ab78a882e111fb45021daac9
The Mimix Company <code@mimix.io>
2020-02-04 21:45:33 +0800

    Implement alternate reader

--- reader.lisp
+++ reader.lisp
@@ -16,6 +16,9 @@
 (mof:defcon +bang+ #\!)
 (mof:defcon +tilde+ #\~)
 (mof:defcon +dollar+ #\$)
+(mof:defcon +space+ #\Space)
+(mof:defcon +left-paren+ #\()
+(mof:defcon +right-paren+ #\))
 
 (defun |[-reader| (stream char)
   "Use [\"/tmp/file.ext\"] as a shorthand for #P\"/tmp/file.ext\""
@@ -47,3 +50,42 @@
   (list 'streams/core:dump (list 'quote (read stream t nil t))))
 
 (set-macro-character +tilde+ #'|~-reader|)
+
+(defun read-next-object (separator delimiter &optional (input-stream *standard-input*))
+  "Read the next lisp object from INPUT-STREAM while using SEPARATOR as token separator and DELIMITER as the end of expression."
+  (flet ((peek-next-char () (peek-char t input-stream t nil t))
+         (discard-next-char () (read-char input-stream t nil t)))
+    (if (and delimiter (char= (peek-next-char) delimiter))
+        (progn (discard-next-char)
+               nil)
+        (let* ((object (read input-stream t nil t))
+               (next-char (peek-next-char)))
+          (cond ((char= next-char separator) (discard-next-char))
+                ((and delimiter (char= next-char delimiter)) nil)
+                (t (error "Unexpected next char: ~S" next-char)))
+          object))))
+
+(defun read-separator (stream char)
+  (declare (ignore stream))
+  (error "Separator ~S shouldn't be read alone" char))
+
+(defun read-left-paren (stream char)
+  (declare (ignore char))
+  (let ((*readtable* (copy-readtable)))
+    (set-macro-character +space+ 'read-separator)
+    (loop :for object = (read-next-object +space+ +right-paren+ stream)
+          :while object
+          :collect object :into objects
+          :finally (return `(vector ,@objects)))))
+
+(defun read-delimiter (stream char)
+  (declare (ignore stream))
+  (error "Delimiter ~S shouldn't be read alone" char))
+
+;;; (set-macro-character +left-paren+ #'read-left-paren)
+;;; (set-macro-character +right-paren+ #'read-delimiter)
+
+(defreadtable streams/syntax
+  (:merge :standard)
+  (:macro-char #\? (constantly nil) t)
+  (:case :preserve))
