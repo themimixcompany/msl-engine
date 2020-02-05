9c1dbb91d58b89b96d4b8b931fff9dcee6a76030
Rommel MARTINEZ <rom@mimix.io>
2020-02-05 18:28:51 +0800

    Use maxpc instead of esrap

--- expr.lisp
+++ expr.lisp
@@ -1,9 +1,11 @@
 ;;;; expr.lisp
 
 (uiop:define-package #:streams/expr
-    (:use #:cl #:esrap)
+    (:use
+     #:cl #:maxpc)
   (:nicknames #:s/expr)
-  (:export #:parse-expr))
+  (:export #:=sexp
+           #:parse-expr))
 
 (in-package #:streams/expr)
 
@@ -14,50 +16,47 @@
   (when (find-if-not #'digit-char-p string)
     t))
 
-(defun msl-char-p (char)
-  "Return true if char is a valid msl character."
-  (or (alphanumericp char)
-      (when (member char '(#\@ #\, #\- #\:))
-        t)))
+(defun extra-chars-p (char)
+  "Return true if CHAR is one of non-standand characters."
+  (when (member char '(#\@ #\, #\- #\:))
+    t))
 
-(defrule whitespace (+ (or #\space #\tab #\newline))
-  (:constant nil))
+(defun ?alphanumeric ()
+  (%or (?satisfies 'alphanumericp)
+       (?satisfies 'extra-chars-p)))
 
-;;; (defrule alphanumeric (alphanumericp character))
-(defrule alphanumeric (msl-char-p character))
+(defun ?string-char ()
+  (%or (?seq (?eq #\\) (?eq #\"))
+       (?satisfies 'not-doublequote)))
 
-(defrule string-char (or (not-doublequote character) (and #\\ #\")))
+(defun =atom ()
+  (%or (=string) (maxpc.digit:=integer-number) (=symbol)))
 
-;;; An expr is either a list or an atom, with possibly leading whitespace.
+(defun =string ()
+  (=destructure (_ s _)
+      (=list (?eq #\")
+             (=subseq (%any (?string-char)))
+             (?eq #\"))))
 
-(defrule expr (and (? whitespace) (or magic list atom))
-  (:function second)
-  (:lambda (s &bounds start end)
-    (list s (cons start end))))
+(defun =symbol ()
+  (=transform (=subseq (?satisfies 'not-integer
+                                   (=subseq (%some (?alphanumeric)))))
+              'intern))
 
-(defrule magic "b612"
-  (:constant :magic)
-  (:when (eq * :use-magic)))
+(defun =sexp ()
+  (%or '=slist/parser (=atom)))
 
-(defrule list (and #\( expr (* expr) (? whitespace) #\))
-  (:destructure (p1 car cdr w p2)
-                (declare (ignore p1 p2 w))
-                (cons car cdr)))
+(defun =slist ()
+  (=destructure (_ expressions _ _)
+      (=list (?eq #\()
+             (%any (=destructure (_ expression)
+                       (=list (%any (maxpc.char:?whitespace)) '=sexp/parser)))
+             (%any (maxpc.char:?whitespace))
+             (?eq #\)))))
 
-(defrule atom (or string integer symbol))
-
-(defrule string (and #\" (* string-char) #\")
-  (:destructure (q1 string q2)
-                (declare (ignore q1 q2))
-                (text string)))
-
-(defrule integer (+ (or "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
-  (:lambda (list)
-    (parse-integer (text list) :radix 10)))
-
-(defrule symbol (not-integer (+ alphanumeric))
-  (:lambda (list)
-    (intern (text list))))
+(setf (fdefinition '=sexp/parser) (=sexp)
+      (fdefinition '=slist/parser) (=slist))
 
 (defun parse-expr (expr)
-  (parse 'expr expr))
+  "Parse expr according the rules listed above."
+  (parse expr (=sexp)))
