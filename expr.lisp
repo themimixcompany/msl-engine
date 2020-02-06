d796ddb0e858a888704840ad92abf8da581288c1
Rommel MARTINEZ <rom@mimix.io>
2020-02-06 21:27:13 +0800

    Enable extraction of pseudo keys

--- expr.lisp
+++ expr.lisp
@@ -16,15 +16,28 @@
   (when (find-if-not #'digit-char-p string)
     t))
 
-(defun extra-chars-p (char)
-  "Return true if CHAR is one of non-standand characters."
-  (when (member char '(#\@ #\, #\- #\:))
-    t))
+(defun inp (integer start &optional end)
+  "Return true if INTEGER is within START and END, inclusively."
+  (if end
+      (and (>= integer start)
+           (<= integer end))
+      (= integer start)))
 
-(defun ?alphanumeric ()
-  "Return a parser that checks if an argument are letters, or extra characters."
+(defun extra-char-p (char)
+  "Return true if CHAR is one of supplementary characters."
+  (let ((code (char-code char)))
+    (or (inp code #x21)
+        (inp code #x23 #x27)
+        (inp code #x2A #x2F)
+        (inp code #x3A #x40)
+        (inp code #x5B #x60)
+        (inp code #x7B #x7E)
+        (>= code #x7F))))
+
+(defun ?msl-char-p ()
+  "Return a parser that checks if an argument are letters, numbers, or extra characters."
   (%or (?satisfies 'alphanumericp)
-       (?satisfies 'extra-chars-p)))
+       (?satisfies 'extra-char-p)))
 
 (defun ?string-char ()
   "Return a parser that checks if an argument is the string marker."
@@ -45,7 +58,7 @@
 (defun =symbol ()
   "Return a parser that checks if an argument is a symbol."
   (=transform (=subseq (?satisfies 'not-integer
-                                   (=subseq (%some (?alphanumeric)))))
+                                   (=subseq (%some (?msl-char-p)))))
               'intern))
 
 (defun =sexp ()
