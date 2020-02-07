38f52cad4d4ea7f42425546cbee0c19ad2fe961a
The Mimix Company <code@mimix.io>
2020-02-07 18:03:28 +0800

    Clean up tests

--- tests.lisp
+++ tests.lisp
@@ -16,26 +16,26 @@
   "Run all the tests defined in the suite."
   (run! 'all-tests))
 
+(test eval-expr-tests
+  "Test the values returned by EVAL-EXPR."
+  (is (null nil)))
+
 (test show-tests
   "Test the output of SHOW."
-  (is (string= $(@walt "Walt" "Disney" :number 1 2 :species "Human" :state "IL") "Walt Disney"))
   (is (string= $"(@walt Walt Disney :number 1 2 :species Human :state IL)" "Walt Disney"))
-  (is (null $(@walt "Walt" "Disney" :number 1 2 :species (@person) :state "IL")))
-  (is (null $(@walt (@nothing))))
-  (is (string= $(@walt) "Walt Disney"))
-  (is (string= $(@walt "WD" :number 0) "WD"))
-  (is (string= $(@walt "XD" :number) "XD"))
-  (is (string= $(@walt :number 1 :age 20 :gender m) "XD"))
-  (is (null $(@david)))
-  (is (null $(@david :number 0 :age 21 :gender m)))
-  (is (string= $(@walt :number) "1"))
-  (is (string= $(@walt :number :state) "XD"))
-  (is (string= $(@walt :number :state "NY") "XD"))
+  (is (null $"(@walt Walt Disney :number 1 2 :species (@person) :state IL)"))
+  (is (null $"(@walt Walt Disney :number 1 2 :species (@person) :state IL)"))
+  (is (null $"(@walt (@nothing))"))
+  (is (string= $"(@walt)" "Walt Disney"))
+  (is (string= $"(@walt WD :number 0)" "WD"))
+  (is (string= $"(@walt XD :number)" "XD"))
+  (is (string= $"(@walt :number 1 :age 20 :gender m)" "XD"))
+  (is (null $"(@david)"))
+  (is (null $"(@david :number 0 :age 21 :gender m)"))
+  (is (string= $"(@walt :number)" "1"))
+  (is (string= $"(@walt :number :state)" "XD"))
+  (is (string= $"(@walt :number :state NY)" "XD"))
   (is (string= $"(@Houston Houston :state TX :nickname H-town)" "Houston"))
   (is (string= $"(@Houston:state)" "TX"))
   (is (string= $"(@energy-corridor An area in (@Houston), (@Houston :state) known as (@Houston :nickname The Energy Capital) concerned with petroleum exploration and drilling!)"
                "An area in Houston , TX known as The Energy Capital concerned with petroleum exploration and drilling!")))
-
-(test eval-expr-tests
-  "Test the values returned by EVAL-EXPR."
-  (is (null nil)))
