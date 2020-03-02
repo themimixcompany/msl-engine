f60a393dc89e25f10b9485474227495a5d059704
The Mimix Company <code@mimix.io>
2020-03-02 08:55:29 -0600

    tiny-atom and atom-getter

--- expr.lisp
+++ expr.lisp
@@ -2,84 +2,12 @@
 
 (uiop:define-package #:streams/expr
     (:use #:cl #:maxpc)
-  (:nicknames #:s/expr)
-  (:export #:=sexp))
+  (:nicknames #:s/expr))
 
 (in-package #:streams/expr)
 
-;;;-----------------------------------------------------------------------------
-;;; Old tokenizer
-;;;-----------------------------------------------------------------------------
 
-(defun not-doublequote (char)
-  "Return true if CHAR is not a double quote (U+0022)"
-  (not (eql #\" char)))
 
-(defun not-integer (string)
-  "Return true if STRING is not an integer."
-  (when (find-if-not #'digit-char-p string)
-    t))
-
-(defun inp (integer start &optional end)
-  "Return true if INTEGER is within START and END, inclusively."
-  (if end
-      (and (>= integer start)
-           (<= integer end))
-      (= integer start)))
-
-(defun extra-char-p (char)
-  "Return true if CHAR is one of supplementary characters."
-  (let ((code (char-code char)))
-    (or (inp code #x21 #x22)            ; #\! #\"
-        (inp code #x23 #x27)            ; #\# #\$ #\% #\& #\'
-        (inp code #x2A #x2F)            ; #\* #\+ #\, #\- #\. #\/
-        (inp code #x3A #x40)            ; #\: #\; #\< #\= #\> #\? #\@
-        (inp code #x5B #x60)            ; #\[ #\\ #\] #\^ #\_ #\`
-        (inp code #x7B #x7E)            ; #\{ #\| #\} #\~
-        (>= code #x7F))))               ; other characters
-
-(defun ?msl-char-p ()
-  "Return a parser that checks if an argument are letters, numbers, or extra characters."
-  (%or (?satisfies 'alphanumericp)
-       (?satisfies 'extra-char-p)))
-
-(defun =symbol ()
-  "Return a parser that checks if an argument is a symbol."
-  (=transform (=subseq (?satisfies 'not-integer (=subseq (%some (?msl-char-p)))))
-              'intern))
-
-(defun =satom ()
-  "Return a parser that checks if an argument is an atom."
-  (%or (maxpc.digit:=integer-number)
-       (=symbol)))
-
-(defun =sexp ()
-  "Return a parser for handling s-expressions."
-  (%or '=slist/parser (=satom)))
-
-(defun =slist ()
-  "Return a parser for handling s-expressions in parens."
-  (=destructure (_ expressions _ _)
-      (=list (?eq #\()
-             (%any (=destructure (_ expression)
-                       (=list (%any (maxpc.char:?whitespace))
-                              '=sexp/parser)))
-             (%any (maxpc.char:?whitespace))
-             (?eq #\)))))
-
-(setf (fdefinition '=sexp/parser) (=sexp)
-      (fdefinition '=slist/parser) (=slist))
-
-;;;-----------------------------------------------------------------------------
-;;; New tokenizer
-;;;-----------------------------------------------------------------------------
-
-;;
-;; Function-namespace definitions for recursive functions
-;;
-
-(setf (fdefinition '=msl-value/parser) (=msl-value)
-      (fdefinition '=msl-atom/parser) (=msl-atom))
 ;;
 ;; Utility (Non-Parser) Functions
 ;;
@@ -113,7 +41,7 @@
 ;; STREAM Single-Value Getters
 
 (defun =sha256 ()
-  "Return a SHA-256 string."
+  "Match and return a SHA-256 string."
   (=subseq (?seq (?satisfies 'length-64-p
                    (=subseq (%some (?hexp)))))))
 ;;
@@ -126,7 +54,7 @@
 
 ;; Dummy Filespec
 (defun =msl-filespec ()
- "Match a return a uri filespec or url."
+ "Match and return a URI filespec or URL."
  (=subseq (%some (?satisfies 'alphanumericp))))
 
 (defun =bracketed-transform ()
@@ -206,6 +134,8 @@
     "Match and return the @ namespace."
     (=subseq (?eq #\@)))
 
+
+
 (defun =msl-atom ()
   "Match and return an atom."
   (=destructure (_ ns-and-key value selector hash comment _)
@@ -219,8 +149,7 @@
                   (list ns key)))
            (%maybe (=destructure (_ value)
                      (=list (?whitespace)
-                            (%or '=msl-atom/parser
-                                 (=msl-value)))))
+                            (%or (=msl-value)))))
            (%any (=destructure (_ value)
                      (=list (?whitespace)
                             (=msl-selector))))
@@ -232,3 +161,46 @@
                             (=msl-comment))))
            (?eq #\right_parenthesis))
     (list ns-and-key value selector hash comment)))
+
+(defun =single-getter ()
+  "Return the components for a single mx-read operation."
+  (%or (=atom-getter)))
+
+;
+
+(defun =metadata-selector ()
+  "Match and return the key sequence for a : selector."
+  ())
+
+
+(defun =atom-getter ()
+  "Match and return the key sequence for an atom."
+  (=list (=destructure (ns _)
+           (%or (=list (=@-namespace)
+                  (%maybe (?whitespace)))
+                (=list (=atom-namespace)
+                  (?whitespace))))
+         (=msl-key)))
+;
+
+(defun =metadata-namespace ()
+  "Match and return the : namespace."
+  (=subseq (?eq #\:)))
+
+;
+(defun =tiny-atom ()
+  "Match and return an atom."
+  (=destructure (_ getter-setter _ _)
+    (=list (?eq #\left_parenthesis)
+           (=single-getter)
+           (?eq #\right_parenthesis)
+           (?end))))
+
+;;
+
+;;
+;; Function-namespace definitions for recursive functions
+;;
+
+(setf (fdefinition '=msl-value/parser) (=msl-value)
+      (fdefinition '=msl-atom/parser) (=msl-atom))
