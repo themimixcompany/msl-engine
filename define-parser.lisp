7f80fe8e9d64c543f8a369c6dbf38a2f985d8323
Rommel MARTINEZ <rom@mimix.io>
2020-03-24 15:43:03 +0800

    Update dispatch system
    - dispatcher: handle sub-atom paths correctly
    - define-parser:new file

---
+++ define-parser.lisp
@@ -0,0 +1,12 @@
+;;;; define-parser.lisp
+
+(in-package #:stream)
+
+(defmacro define-parser (name args &body body)
+  "Define a function for defining parsers. NAME is the name of the parser
+function; ARGS are the arguments passed to a parser—usually NIL; and BODY is the
+body contents of the parser function."
+  `(prog1
+     (defun ,name ,args
+       ,@body)
+     (setf (fdefinition ',(intern (subseq (string name) 1)) (,name)))))
