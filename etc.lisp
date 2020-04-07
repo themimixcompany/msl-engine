c1b438754e1f393aa87046662a3165a02b4a57ca
The Mimix Company <code@mimix.io>
2020-04-07 10:25:58 +0800

    etc: refactor

--- etc.lisp
+++ etc.lisp
@@ -7,16 +7,15 @@
   (:export #:dump-object
            #:slots
            #:dump-universe
-           #:dump-atom
            #:dump-table
            #:dump-path
-           #:tables
            #:clear-table
+           #:tables
            #:clear-universe
+           #:copy-universe
            #:copy-table
            #:clear-path
-           #:filter-path
-           #:copy-universe))
+           #:filter-path))
 
 (in-package #:streams/etc)
 
@@ -50,19 +49,23 @@
                 (marie:dump-table* table)))))
 
 (defun dump-table (table)
-  "Print information about SOURCE recursively."
+  "Print information about TABLE recursively."
   (marie:dump-table* table))
 
-(defun dump-path (path &optional (table (atom-table *universe*)))
-  "Dump table information from PATH starting with TABLE."
+(defun dump-path (table path)
+  "Print the information in TABLE specified by PATH."
   (cond ((marie:solop path)
          (multiple-value-bind (val existsp)
              (gethash (marie:stem path) table)
            (when (and (hash-table-p val) existsp)
              (dump-table val))))
         ((hash-table-p (gethash (car path) table))
-         (dump-path (cdr path) (gethash (car path) table)))
+         (dump-path (gethash (car path) table) (cdr path)))
         (t nil)))
+
+(defun clear-table (table)
+  "Clear all the contents of TABLE."
+  (clrhash table))
 
 (defun tables (&optional (universe *universe*))
   "Return the list of slots from UNIVERSE that are tables."
@@ -71,59 +74,9 @@
         :when (hash-table-p tab)
         :collect tab))
 
-(defun clear-table (table)
-  "Clear all the contents of TABLE."
-  (clrhash table))
-
 (defun clear-universe (&optional (universe *universe*))
   "Set the current universe to an empty state."
   (loop :for table :in (tables universe) :do (clear-table table)))
-
-(defun copy-table (table)
-  "Create a new hash table from TABLE."
-  (let ((ht (make-hash-table :test (hash-table-test table)
-                             :rehash-size (hash-table-rehash-size table)
-                             :rehash-threshold (hash-table-rehash-threshold table)
-                             :size (hash-table-size table))))
-    (loop :for key :being :the :hash-key :of table
-          :using (hash-value value)
-          :do (setf (gethash key ht) value)
-          :finally (return ht))))
-
-(defun clear-path (source path &key (copy t))
-  "Remove the key specified under PATH in a copy of SOURCE. If COPY is false,
-modify SOURCE in-place."
-  (let ((table (if copy (copy-table source) source)))
-    (labels ((fn (ht location)
-               (cond ((marie:solop location)
-                      (remhash (marie:stem location) ht)
-                      table)
-                     ((hash-table-p (gethash (car location) ht))
-                      (fn (gethash (car location) ht) (cdr location)))
-                     (t nil))))
-      (fn table path))))
-
-(defun clear-other (table key)
-  "Remove entries in TABLE that do not match KEY."
-  (let ((keys (loop :for k :being :the :hash-key :of table :collect k)))
-    (loop :for item :in (remove key keys :test #'equal)
-          :do (remhash item table)
-          :finally (return table))))
-
-(defun filter-path (source path &key (copy t))
-  "Return a copy of table SOURCE with only the keys in PATH. If COPY is false,
-modify SOURCE in-place."
-  (let ((table (if copy (copy-table source) source)))
-    (labels ((fn (ht location)
-               (cond ((and (marie:solop location)
-                           (hash-table-p (gethash (car location) ht)))
-                      (clear-other ht (car location))
-                      table)
-                     ((hash-table-p (gethash (car location) ht))
-                      (clear-other ht (car location))
-                      (fn (gethash (car location) ht) (cdr location)))
-                     (t nil))))
-      (fn table path))))
 
 (defun copy-universe (universe)
   "Return a copy of the universe UNIVERSE, but with a new log date. The
@@ -137,3 +90,44 @@ copying."
                    :sub-atom-counter sub-atom-counter
                    :sub-atom-table (copy-table sub-atom-table))))
 
+(defun copy-table (table)
+  "Create a new hash table from TABLE."
+  (let ((ht (make-hash-table :test (hash-table-test table)
+                             :rehash-size (hash-table-rehash-size table)
+                             :rehash-threshold (hash-table-rehash-threshold table)
+                             :size (hash-table-size table))))
+    (loop :for key :being :the :hash-key :of table
+          :using (hash-value value)
+          :do (setf (gethash key ht) value)
+          :finally (return ht))))
+
+(defun clear-path (table path)
+  "Remove the specified entry in TABLE that matches PATH."
+  (labels ((fn (tab location)
+             (cond ((marie:solop location)
+                    (remhash (marie:stem location) tab)
+                    table)
+                   ((hash-table-p (gethash (car location) tab))
+                    (fn (gethash (car location) tab) (cdr location)))
+                   (t nil))))
+    (fn table path)))
+
+(defun clear-other (table key)
+  "Remove entries in TABLE that do not match KEY."
+  (let ((keys (loop :for k :being :the :hash-key :of table :collect k)))
+    (loop :for item :in (remove key keys :test #'equal)
+          :do (remhash item table)
+          :finally (return table))))
+
+(defun filter-path (table path)
+  "Remove all other table entries in SOURCE that do not match PATH."
+  (labels ((fn (tab location)
+               (cond ((and (marie:solop location)
+                           (hash-table-p (gethash (car location) tab)))
+                      (clear-other tab (car location))
+                      table)
+                     ((hash-table-p (gethash (car location) tab))
+                      (clear-other tab (car location))
+                      (fn (gethash (car location) tab) (cdr location)))
+                     (t nil))))
+    (fn table path)))
