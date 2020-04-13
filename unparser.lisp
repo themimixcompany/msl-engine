1da8e1f4582348a17bbe1155b47d2bc920dbe0a9
The Mimix Company <code@mimix.io>
2020-04-14 00:30:27 +0800

    unparser: refactor

--- unparser.lisp
+++ unparser.lisp
@@ -4,17 +4,16 @@
   (:use #:cl
         #:streams/specials
         #:streams/classes)
-  (:export #:prefixedp
-           #:combine
-           #:normalize
-           #:compose
-           #:flatten-1
+  (:export #:metadatap
            #:datatype-format-p
-           #:foo
-           #:bar
-           #:baz
-           #:qux
-           #:sub-flatten-1
+           #:prefixedp
+           #:flatten-1
+           #:wrap
+           #:join
+           #:stage
+           #:normalize
+           #:combine
+           #:compose
            #:construct
            #:collect))
 
@@ -39,47 +38,12 @@
             :when (hash-table-p value)
             :collect (if object value key)))))
 
-(defun colonp (value)
+(defun metadatap (value)
   "Return true if VALUE is the : namespace."
   (when (consp value)
     (let ((value (car value)))
       (and (stringp value)
            (string= value ":")))))
-
-(defun prefixedp (value)
-  "Return true if VALUE is prefixed by certain namespaces."
-  (marie:when*
-    (consp value)
-    (member (car value) '(":" "d" "f") :test #'equal)))
-
-(defun merge-heads (value)
-  "Return a list where specific namespace ids are merged with its keys."
-  (labels ((fn (val)
-             (if (prefixedp val)
-                 (cond ((colonp val)
-                        (cons (marie:cat (first val) (second val))
-                              (cddr val)))
-                       (t val))
-                 val)))
-    (cond ((consp value) (fn value))
-          (t value))))
-
-(defun flatten-1 (list)
-  "Return a flattened list on one level from LIST."
-  (let ((value (mapcar #'(lambda (item)
-                           (if (consp item)
-                               item
-                               (list item))
-                           ;; (cond ((or (stringp item)
-                           ;;            (and (consp item) (consp (car item))))
-                           ;;        (list item))
-                           ;;       (t item))
-                           )
-                       list)))
-    (reduce #'(lambda (x y)
-                (cond ((prefixedp y) (append x (list y)))
-                      (t (append x y))))
-            value)))
 
 (defun datatype-format-p (value)
   "Return true if VALUE is a datatype or format form."
@@ -89,54 +53,68 @@
            (or (string= value "d")
                (string= value "f"))))))
 
-(defun foo (list)
+(defun prefixedp (value)
+  "Return true if VALUE is prefixed by certain namespaces."
+  (marie:when*
+    (consp value)
+    (member (car value) '(":" "d" "f") :test #'equal)))
+
+(defun flatten-1 (list)
+  "Return a flattened list on one level from LIST."
+  (let ((value (mapcar #'(lambda (item)
+                           (if (consp item) item (list item)))
+                       list)))
+    (reduce #'(lambda (x y)
+                (cond ((prefixedp y) (append x (list y)))
+                      (t (append x y))))
+            value)))
+
+(defun wrap (list)
+  "Return a new list where items in LIST are conditionally listified."
   (mapcar #'(lambda (item)
               (cond ((or (atom item)
                          (and (consp item)
                               (not (prefixedp item))
                               (not (stringp (car item)))))
                      (list item))
-                    ((colonp item)
+                    ((metadatap item)
                      (cons (marie:cat (first item) (second item))
                            (cddr item)))
                     (t item)))
           list))
 
-(defun bar (list)
+(defun join (list)
+  "Return a new list where items in LIST are flattened to one level."
   (reduce #'(lambda (x y)
               (cond ((datatype-format-p y) (append x (list y)))
                     ((datatype-format-p x) (append (list x) y))
                     (t (append x y))))
           list))
 
-(defun baz (list)
+(defun stage (list)
+  "Return a new list from LIST where the items preprocessed for wrapping and joining."
   (labels ((fn (args acc)
              (cond ((null args) (nreverse acc))
                    ((datatype-format-p (car args))
-                    (fn (cdr args) (cons (flatten-1 (car args)) acc)))
-                   ((colonp (car args))
                     (fn (cdr args)
-                        (cons (bar (foo (fn (car args) nil)))
+                        (cons (flatten-1 (car args)) acc)))
+                   ((metadatap (car args))
+                    (fn (cdr args)
+                        (cons (join (wrap (fn (car args) nil)))
                               acc)))
-                   (t (fn (cdr args) (cons (car args) acc))))))
+                   (t (fn (cdr args)
+                          (cons (car args) acc))))))
     (fn list nil)))
 
-(defun qux (list)
-  (bar (foo (baz list))))
-
-(defun sub-flatten-1 (value)
-  "Return flattening on sub items of LIST."
-  ;; (mapcar #'(lambda (item)
-  ;;             (cond ((prefixedp item)
-  ;;                    (flatten-1 item))
-  ;;                   (t item)))
-  ;;         value)
-  (cond ((prefixedp value)
-         (mapcar #'(lambda (item)
-                     (cond ((prefixedp item) (flatten-1 item))
-                           (t item)))
-                 value))
-        (t value)))
+(defun normalize (list)
+  "Return special merging on items of LIST."
+  (labels ((fn (val)
+             (cond ((metadatap val) (cons (car val) (cadr val)))
+                   (t val))))
+    (join (wrap (stage (mapcar #'(lambda (item)
+                                   (cond ((consp item) (fn item))
+                                         (t item)))
+                               list))))))
 
 (defun %combine (list)
   "Return the list (X Y ...) from (X (Y ...)) from LIST."
@@ -154,7 +132,7 @@
   "Apply additional merging operations to items in LIST."
   (labels ((fn (args acc)
              (cond ((null args) (nreverse acc))
-                   ((colonp (car args))
+                   ((metadatap (car args))
                     (fn (cdr args)
                         (cons (list (caar args)
                                     (combine (cadr (car args))))
@@ -163,14 +141,6 @@
                           (cons (%combine (car args))
                                 acc))))))
     (fn items nil)))
-
-(defun normalize (list)
-  "Return special merging on items of LIST."
-  (labels ((fn (val)
-             (cond ((colonp val) (cons (car val) (cadr val)))
-                   (t val))))
-    (cond ((consp list) (fn list))
-          (t list))))
 
 (defun accumulate (value acc)
   "Return an an accumulator value suitable for CONSTRUCT."
@@ -198,12 +168,7 @@
     (marie:when-let ((ht (gethash key table)))
       (loop :for v :in (fn ht (table-keys ht) nil)
             :for kv = (cons key v)
-            ;;:collect kv
-            ;;:collect (flatten-1 (compose kv))
-            :collect (mapcar #'normalize (compose kv))
-            ;;:collect (mapcar #'sub-flatten-1 (mapcar #'normalize (compose kv)))
-            ;;:collect (flatten-1 (mapcar #'sub-flatten-1 (mapcar #'normalize (compose kv))))
-            ))))
+            :collect (normalize (compose kv))))))
 
 (defun collect (&optional (table (atom-table *universe*)))
   "Return the original expressions in TABLE."
