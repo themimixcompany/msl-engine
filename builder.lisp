04d0682a8d45d15247ed2eef7434b25bc0c91d55
The Mimix Company <code@mimix.io>
2020-04-28 18:52:24 +0800

    builder: refactor

--- builder.lisp
+++ builder.lisp
@@ -2,35 +2,32 @@
 
 (uiop:define-package #:streams/builder
   (:use #:cl
-        #:marie)
-  (:export #:build))
+        #:marie))
 
 (in-package #:streams/builder)
 
-(defgeneric build (&optional root)
-  (:documentation "Build the executable of streams for different platforms."))
-
-(defmethod build :before (&optional root)
-  (declare (ignorable root))
-  (muffle-debugger))
-
-(defmethod build (&optional (root *default-pathname-defaults*))
-  (let ((arch (string (uiop:architecture))))
-    (labels ((make-name (name)
+(defun* (build t) (&optional (root *default-pathname-defaults*))
+  "Build the executable of streams for different platforms."
+  (with-muffled-debugger
+    (let ((arch (string (uiop:architecture))))
+      (flet ((fn (name)
                (let ((suffix (cond ((string= name "windows") (cat name "_" arch ".exe"))
                                    (t (cat name "_" arch)))))
                  (cat "streams_" suffix))))
-      (let* ((base-name (cond ((uiop:os-macosx-p) (make-name "macos"))
-                              ((uiop:os-windows-p) (make-name "windows"))
-                              ((uiop:os-unix-p) (make-name "unix"))
-                              (t (error "No matching OS found."))))
-             (path (uiop:subpathname* root base-name)))
-        (uiop:ensure-all-directories-exist (list (namestring path)))
-        #+sbcl
-        (sb-ext:save-lisp-and-die path :toplevel #'streams/server:serve :executable t :compression nil)
-        #+ccl
-        (ccl:save-application path :toplevel-function #'streams/server:serve :prepend-kernel t)
-        #+clisp
-        (ext:saveinitmem path :init-function #'(lambda () (funcall 'streams/server:serve)
-                                                 (ext:exit))
-                              :executable t :norc t)))))
+        (let* ((base-name (cond ((uiop:os-macosx-p) (fn "macos"))
+                                ((uiop:os-windows-p) (fn "windows"))
+                                ((uiop:os-unix-p) (fn "unix"))
+                                (t (error "No matching OS found."))))
+               (path (uiop:subpathname* root base-name)))
+          (uiop:ensure-all-directories-exist (list (namestring path)))
+          #+sbcl
+          (sb-ext:save-lisp-and-die path :toplevel #'streams/server:serve
+                                         :executable t
+                                         :compression nil)
+          #+ccl
+          (ccl:save-application path :toplevel-function #'streams/server:serve
+                                     :prepend-kernel t)
+          #+clisp
+          (ext:saveinitmem path :init-function #'(lambda () (funcall 'streams/server:serve) (ext:exit))
+                                :executable t
+                                :norc t))))))
