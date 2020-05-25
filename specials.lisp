aa512259328ceb81ba472021d68d40b47c6683ab
The Mimix Company <code@mimix.io>
2020-05-25 22:44:43 +0800

    specials: hard code version number

--- specials.lisp
+++ specials.lisp
@@ -70,5 +70,6 @@
   "The default name to use as the machine name.")
 
 (defvar* *system-version*
-  #.(asdf:system-version (asdf:find-system :streams))
+  ;#.(asdf:system-version (asdf:find-system :streams))
+  "2.2.0"
   "The introspected version of this system.")
