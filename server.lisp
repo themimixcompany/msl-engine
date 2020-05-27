bf772c4ebe9deceb5ac1c91d7334f27739ea2bfe
The Mimix Company <code@mimix.io>
2020-05-27 18:43:11 +0800

    server: dispatch (@VER) properly

--- server.lisp
+++ server.lisp
@@ -49,7 +49,6 @@
   (let ((uid (get-new-user-id)))
     (setf (gethash connection *user-connections*)
           (format nil "~A" uid))
-    ;; (post connection (admin-dispatch "(@VERSION)"))
     ;;(send connection (format-msl "VER" *system-version*))
     (post connection (admin-dispatch "(@VER)"))))
 
