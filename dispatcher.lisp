cde0d7f85db838407763e4fea1d363ad6c7d55bb
Rommel MARTINEZ <rom@mimix.io>
2020-06-23 17:52:33 +0800

    dispatcher: create checkpoint

--- dispatcher.lisp
+++ dispatcher.lisp
@@ -33,7 +33,7 @@
     (declare (ignore _))
     (sub-namespace-p ns)))
 
-(defun sub-atom-path-p* (path)
+(defun* with-sub-atom-path-p (path)
   "Retun true if PATH contains a sub-atom path and PATH is not a sub-atom path itself."
   (when* (sub-atom-index path) (not (sub-atom-path-p path))))
 
@@ -71,11 +71,11 @@
 (defun save-value (term location table value)
   "Store VALUE using LOCATION as key in TABLE."
   (declare (ignorable term))
-  (let ((v (cond ((sub-atom-path-p* value) (sub-atom-path value))
-                   (t value))))
-      (when (mem* location '("/" "[]"))
-        ;;(clear-path table head)
-        nil)
+  (let ((v (if (with-sub-atom-path-p value)
+               (sub-atom-path value)
+               value)))
+    ;; (when (mem* location '("/" "[]"))
+    ;;   (clear-path table head))
     (setf (gethash (single location) table) v)))
 
 (defun spawn-table (location table)
@@ -86,37 +86,109 @@
         (setf (gethash (car location) table) ht)
         ht)))
 
-(defun write-term (term atom-table sub-atom-table &key whole)
+;; (defun write-term (term atom-table sub-atom-table &key whole)
+;;   "Return a hash table containing the embedded value tables as specified in TERM."
+;;   (destructuring-bind (path &optional params)
+;;       term
+;;     (labels ((fn (location flag atom-tab sub-atom-tab)
+;;                (cond ((null location)
+;;                       (fn '("=") flag atom-tab sub-atom-tab))
+;;                      ((and (singlep location) (key-indicator-p (single location)))
+;;                       (save-value term location atom-tab (if whole params (car params)))
+;;                       (when flag
+;;                         (fn (sub-atom-path path) nil sub-atom-tab sub-atom-tab)))
+;;                      (t (fn (cdr location) flag (spawn-table location atom-tab) sub-atom-tab)))))
+;;       (fn path (with-sub-atom-path-p path) atom-table sub-atom-table)
+;;       (read-term term atom-table sub-atom-table))))
+
+(defun* write-term (term atom-table sub-atom-table &key whole)
   "Return a hash table containing the embedded value tables as specified in TERM."
-  (destructuring-bind (path &optional params)
+  (dbg term)
+  (destructuring-bind (path &optional &rest params)
       term
-    (labels ((fn (location flag atom-tab sub-atom-tab)
-               (cond ((null location)
-                      (fn '("=") flag atom-tab sub-atom-tab))
-                     ((and (singlep location) (key-indicator-p (single location)))
-                      (save-value term location atom-tab (if whole params (car params)))
-                      (when flag
-                        (fn (sub-atom-path path) nil sub-atom-tab sub-atom-tab)))
-                     (t (fn (cdr location) flag (spawn-table location atom-tab) sub-atom-tab)))))
-      (fn path (sub-atom-path-p* path) atom-table sub-atom-table)
-      (read-term term atom-table sub-atom-table))))
+    (let ((opt (with-sub-atom-path-p path))
+          (parameters (if whole params (car params))))
+      (labels ((fn (location flag atom-tab sub-atom-tab)
+                 (cond ((and (null location) flag (null parameters))
+                        (dbg "1a")
+                        (values atom-tab
+                                sub-atom-tab))
+
+                       ((and (null location) flag parameters)
+                        (dbg "1b")
+                        (fn '("=") nil atom-tab sub-atom-tab))
+
+                       ;; the normal case
+                       ((and (null location) (not flag))
+                        (dbg "2")
+                        (fn '("=") flag atom-tab sub-atom-tab))
+
+                       ;; the setter
+                       ((and (singlep location)
+                             (key-indicator-p (single location)))
+                        (dbg "3")
+                        (save-value term location atom-tab parameters)
+                        (when flag
+                          (fn (sub-atom-path path) nil sub-atom-tab
+                              ;; maybe pass nil as the second table
+                              sub-atom-tab)))
+
+                       ;; the looper
+                       (t
+                        (dbg "4")
+                        (fn (cdr location) flag (spawn-table location atom-tab) sub-atom-tab)))))
+        (fn path opt atom-table sub-atom-table)
+
+        ;; update READ-TERM to reflect =-less entries
+        ;; (read-term term atom-table sub-atom-table)
+        ))))
 
 (defun empty-params-p (params)
   "Return true if PARAMS is considered empty."
   (or (null params)
       (every #'null params)))
 
-(defun find-table (table)
+(defun* find-table (table)
   "Return the table from the universe identified by TABLE."
   (funcall table *universe*))
+
+;; (defun* dispatch (expr &optional (log t))
+;;   "Evaluate EXPR as an MSL expression and store the resulting object in the universe."
+;;   (flet ((fn (term atom-tab sub-atom-tab)
+;;            (destructuring-bind (path &optional &rest params)
+;;                term
+;;              (cond ((empty-params-p params)
+;;                     (read-term (list path params) atom-tab sub-atom-tab))
+;;                    (t (let ((values (write-term (list path params) atom-tab sub-atom-tab)))
+;;                         (when (consp values)
+;;                           (loop :for value :in values
+;;                                 :when (valid-terms-p value)
+;;                                 :do (dispatch value)))
+;;                         values))))))
+;;     (let ((terms (if (consp expr) expr (parse-msl expr)))
+;;           (atom-tab (find-table #'atom-table))
+;;           (sub-atom-tab (find-table #'sub-atom-table)))
+;;       (when terms
+;;         (when (and log (stringp expr))
+;;           (write-log expr))
+;;         (loop :for term :in terms
+;;               :collect (fn term atom-tab sub-atom-tab))))))
 
 (defun* dispatch (expr &optional (log t))
   "Evaluate EXPR as an MSL expression and store the resulting object in the universe."
   (flet ((fn (term atom-tab sub-atom-tab)
            (destructuring-bind (path &optional &rest params)
                term
-             (cond ((empty-params-p params)
-                    (read-term (list path params) atom-tab sub-atom-tab))
+             (cond ((and (empty-params-p params)
+                         (with-sub-atom-path-p path))
+                    (dbg "X")
+                    ;; (read-term (list path params) atom-tab sub-atom-tab)
+                    )
+                   ((and (empty-params-p params)
+                         (not (with-sub-atom-path-p path)))
+                    (dbg "Y")
+                    ;; (read-term (list path params) atom-tab sub-atom-tab)
+                    )
                    (t (let ((values (write-term (list path params) atom-tab sub-atom-tab)))
                         (when (consp values)
                           (loop :for value :in values
