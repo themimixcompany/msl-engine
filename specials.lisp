87ef52b0565ac0641522dcdc1482885751ad47d6
Rommel MARTINEZ <rom@mimix.io>
2020-06-29 20:26:12 +0800

    streams: bump version number

--- specials.lisp
+++ specials.lisp
@@ -80,7 +80,7 @@
 
 (defvar* *system-version*
   ;;#.(asdf:system-version (asdf:find-system :streams))
-  "2.2.11"
+  "2.2.12"
   "The introspected version of this system.")
 
 (defvar* *slynk-port*
