72993f44c4172893a8ee01fd5c9a11c7a233a643
The Mimix Company <code@mimix.io>
2020-07-01 16:22:50 +0800

    dispatcher: conditionalize logging

--- specials.lisp
+++ specials.lisp
@@ -80,7 +80,7 @@
 
 (defvar* *system-version*
   ;;#.(asdf:system-version (asdf:find-system :streams))
-  "2.2.14"
+  "2.2.15"
   "The introspected version of this system.")
 
 (defvar* *slynk-port*
