e7e377378c3da794e12f0166f25565a7d0ce3848
The Mimix Company <code@mimix.io>
2020-07-01 21:39:11 +0800

    driver: reorder modules

--- driver.lisp
+++ driver.lisp
@@ -1,21 +1,21 @@
 ;;;; driver.lisp
 
 (uiop:define-package :streams/driver
-  (:nicknames :streams)
-  (:use :uiop/common-lisp)
-  (:use-reexport
-   #:streams/specials
-   #:streams/classes
-   #:streams/common
-   #:streams/parser
-   #:streams/unparser
-   #:streams/logger
-   #:streams/dispatcher
-   #:streams/bridge
-   #:streams/admin-dispatcher
-   #:streams/server
-   #:streams/builder
-   #:streams/startup))
+  (:nicknames #:streams)
+  (:use #:uiop/common-lisp
+        #:marie)
+  (:use-reexport #:streams/specials
+                 #:streams/classes
+                 #:streams/common
+                 #:streams/parser
+                 #:streams/logger
+                 #:streams/dispatcher
+                 #:streams/unparser
+                 #:streams/bridge
+                 #:streams/admin-dispatcher
+                 #:streams/server
+                 #:streams/builder
+                 #:streams/startup))
 
 (provide "streams")
 (provide "STREAMS")
