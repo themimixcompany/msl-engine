5de54bea16f45d11f8ffa7cba45e83f31fc03ab8
The Mimix Company <code@mimix.io>
2020-07-06 21:29:51 +0800

    streams: bump version number

--- specials.lisp
+++ specials.lisp
@@ -80,7 +80,7 @@
 
 (defvar* *system-version*
   ;;#.(asdf:system-version (asdf:find-system :streams))
-  "2.2.21"
+  "2.2.22"
   "The introspected version of this system.")
 
 (defvar* *slynk-port*
