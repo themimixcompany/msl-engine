554035e017dbfb7e6d10791268b2c4d02b57889b
The Mimix Company <code@mimix.io>
2020-07-15 19:15:21 +0800

    streams: bump version number

--- specials.lisp
+++ specials.lisp
@@ -103,7 +103,7 @@
     ;; (uiop:os-cond
     ;;  ((uiop:os-windows-p) (system-version +self+))
     ;;  (t (asdf:system-version (system-object +self+))))
-  "2.2.31"
+  "2.2.32"
   "The introspected version of this system.")
 
 (defvar* *slynk-port*
