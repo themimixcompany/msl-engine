593c1877afe5ebfc660ca7dd18ade7c9bb5240a9
The Mimix Company <code@mimix.io>
2020-08-01 12:27:10 +0800

    parser: refactor the metadata parser

--- specials.lisp
+++ specials.lisp
@@ -107,7 +107,7 @@
     ;; (uiop:os-cond
     ;;  ((uiop:os-windows-p) (system-version +self+))
     ;;  (t (asdf:system-version (system-object +self+))))
-  "2.3.16"
+  "2.3.17"
   "The introspected version of this system.")
 
 (defvar* *slynk-port*
