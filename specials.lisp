1312cf598c32e3428afc5a7e358752bf103f096b
Rommel MARTINEZ <rom@mimix.io>
2020-09-08 00:06:32 +0800

    parser: refactor main metadata handler

--- specials.lisp
+++ specials.lisp
@@ -111,7 +111,7 @@
   ;; (uiop:os-cond
   ;;  ((uiop:os-windows-p) (system-version +self+))
   ;;  (t (asdf:system-version (system-object +self+))))
-  "2.4.33"
+  "2.4.34"
   "The introspected version of this system.")
 
 (defv *slynk-port*
