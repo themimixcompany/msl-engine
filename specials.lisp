af1cf159a80061973dc9da148b943f9457ad2dfd
Rommel MARTINEZ <rom@mimix.io>
2020-09-07 18:21:35 +0800

    parser: refactor =REGEX-SELECTOR and =BRACKETED-TRANSFORM-SELECTOR

--- specials.lisp
+++ specials.lisp
@@ -111,7 +111,7 @@
   ;; (uiop:os-cond
   ;;  ((uiop:os-windows-p) (system-version +self+))
   ;;  (t (asdf:system-version (system-object +self+))))
-  "2.4.30"
+  "2.4.31"
   "The introspected version of this system.")
 
 (defv *slynk-port*
