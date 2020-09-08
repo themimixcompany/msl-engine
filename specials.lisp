0bf9c0037b638692f397b92ae7d584b09f29305c
Rommel MARTINEZ <rom@mimix.io>
2020-09-09 00:23:14 +0800

    reader: support bracketed transform value extraction

--- specials.lisp
+++ specials.lisp
@@ -112,7 +112,7 @@
   ;; (uiop:os-cond
   ;;  ((uiop:os-windows-p) (system-version +self+))
   ;;  (t (asdf:system-version (system-object +self+))))
-  "2.4.36"
+  "2.4.37"
   "The introspected version of this system.")
 
 (defv *slynk-port*
